5a14285edd61320c0f95a9110258f4fd06d41b61
Val <gnvl61@gmail.com>
2017-08-10 20:23:38 -0700

    HW_31

---
+++ src/test/java/core/Calculator.java
@@ -0,0 +1,65 @@
+package core;
+
+public class Calculator {
+
+	public static double add(double d, double e) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public static double add(double d, double e, double f) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public static double add(double d, double e, double f, double g) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public static double divide(double d, double e) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public static double divide(double d, double e, double f) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public static double divide(double d, double e, double f, double g) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public static double multiply(double d, double e) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public static double subtract(double d, double e) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public static double subtract(double d, double e, double f) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public static double subtract(double d, double e, double f, double g) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public static double multiply(double d, double e, double f, double g) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public static double multiply(double d, double e, double f) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
